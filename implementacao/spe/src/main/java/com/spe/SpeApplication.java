cdf82235a05c142c3e2ed04f68779fab001605cf
Joanne Lima <joannealima@gmail.com>
2020-12-05 03:32:41 -0300

    criando projeto

---
+++ implementacao/spe/src/main/java/com/spe/SpeApplication.java
@@ -0,0 +1,13 @@
+package com.spe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeApplication.class, args);
+	}
+
+}
